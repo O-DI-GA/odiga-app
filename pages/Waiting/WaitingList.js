de8b716573120985f27f822524afec8c688f0db3
hjoo830 <hjoo830@naver.com>
2024-05-17 18:12:33 +0900

    ✨ Feat: Waiting List

--- pages/Waiting/WaitingList.js
+++ pages/Waiting/WaitingList.js
@@ -2,12 +2,34 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Header from "../../component/Header";
 import NavBar from "../../component/NavBar";
+import ReserveContainer from "../../component/ReserveContainer";
 
 const WaitingList = () => {
   return (
     <View style={styles.container}>
       <Header />
-      <Text>웨이팅 목록 화면</Text>
+      <View style={styles.content}>
+        <Text style={styles.label}>내 웨이팅 정보</Text>
+        <ReserveContainer
+          imageUrl="#"
+          shopName="가게 이름"
+          statusMsg="현재 5팀 남았어요!"
+          onPress={() => console.log("가게 상세 페이지로 이동")}
+        />
+        <ReserveContainer
+          imageUrl="#"
+          shopName="가게 이름"
+          statusMsg="현재 8팀 남았어요!"
+          onPress={() => console.log("가게 상세 페이지로 이동")}
+        />
+        <Text style={styles.label}>내 예약 정보</Text>
+        <ReserveContainer
+          imageUrl="#"
+          shopName="가게 이름"
+          statusMsg="5월 5일 18시"
+          onPress={() => console.log("가게 상세 페이지로 이동")}
+        />
+      </View>
       <NavBar />
     </View>
   );
@@ -17,6 +39,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
+  },
+  content: {
+    padding: 20,
+    paddingTop: 0,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
   },
 });
 
