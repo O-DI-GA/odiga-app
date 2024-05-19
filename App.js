e240d6c12cd97700a256b378facafa94ee5a163c
dajeong <dajung5623@naver.com>
2024-05-19 20:37:16 +0900

    ✨ Feat : 가게 상세페이지 추가

--- App.js
+++ App.js
@@ -12,28 +12,31 @@ import QRScan from "./component/QRScan";
 import Map from "./pages/Map";
 import MyPage from "./pages/User/MyPage";
 
+import ShopDetail from "./pages/Shop/ShopDetail";
+
 const Stack = createStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false, // 헤더 숨김
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="WaitingList" component={WaitingList} />
-        <Stack.Screen name="QRScan" component={QRScan} />
-        <Stack.Screen name="Map" component={Map} />
-        <Stack.Screen name="MyPage" component={MyPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerShown: false, // 헤더 숨김
+                }}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Main" component={Main} />
+                <Stack.Screen name="WaitingList" component={WaitingList} />
+                <Stack.Screen name="QRScan" component={QRScan} />
+                <Stack.Screen name="Map" component={Map} />
+                <Stack.Screen name="MyPage" component={MyPage} />
+
+                <Stack.Screen name="ShopDetail" component={ShopDetail} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
