da8c2b8c1c3ed7a22c96a4ca469d7733fb24b765
dajeong <dajung5623@naver.com>
2024-05-27 19:09:54 +0900

    ✨ Feat : 가게 상세페이지 추가

---
+++ pages/Shop/ShopReview.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ShopReview() {
+    return (
+        <View>
+            <Text>ShopReview</Text>
+        </View>
+    );
+}
