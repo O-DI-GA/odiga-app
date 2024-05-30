34c823cd19ba8ec2c6c44644984b439aba0ccdc5
dajeong <dajung5623@naver.com>
2024-05-30 22:22:39 +0900

    ✨ feat : 가게 리뷰 컴포넌트 생성

--- pages/Shop/ShopReview.js
+++ pages/Shop/ShopReview.js
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import ReviewBox from "../../component/ReviewBox";
 
 export default function ShopReview() {
     return (
         <View>
-            <Text>ShopReview</Text>
+            <ReviewBox />
         </View>
     );
 }
