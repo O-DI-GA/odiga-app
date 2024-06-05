e60ae432338d62189fb52c72dc309ffe76646894
hjoo830 <hjoo830@naver.com>
2024-06-05 23:43:11 +0900

    🐛 Fix: API address

--- utils/api/api.js
+++ utils/api/api.js
@@ -1,3 +1,36 @@
-const URL = "13.125.83.255:8080";
+const URL = "http://13.125.83.255:8080";
+
+// GET
+export const getRequest = async (endpoint) => {
+  try {
+    const response = await fetch(`${URL}/${endpoint}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+// POST
+export const postRequest = async (endpoint, data) => {
+  try {
+    const response = await fetch(`${URL}/${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
 export default URL;
