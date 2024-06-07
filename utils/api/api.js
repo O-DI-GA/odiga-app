26cb5f55d486b860486d6cc5c756f33bedab6a08
hjoo830 <hjoo830@naver.com>
2024-06-07 16:15:00 +0900

    ✨ Feat: Add POST and DELETE API

--- utils/api/api.js
+++ utils/api/api.js
@@ -33,4 +33,37 @@ export const postRequest = async (endpoint, data) => {
   }
 };
 
+// PUT
+export const putRequest = async (endpoint, data) => {
+  try {
+    const response = await fetch(`${URL}/${endpoint}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+// DELETE
+export const deleteRequest = async (endpoint) => {
+  try {
+    const response = await fetch(`${URL}/${endpoint}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
 export default URL;
