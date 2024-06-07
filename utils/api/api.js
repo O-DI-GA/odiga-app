abe859710689d9c7b19f4efa7be72cd1020291e5
dajeong <dajung5623@naver.com>
2024-06-07 20:01:26 +0900

    🐛 chore : Edit code format

--- utils/api/api.js
+++ utils/api/api.js
@@ -1,11 +1,9 @@
-
 import { getTokenFromStroage } from "../useAuthUtils";
 
 const URL = "http://13.125.83.255:8080";
 
 // GET
 export const getRequest = async (endpoint) => {
-
     try {
         const response = await fetch(`${URL}/${endpoint}`, {
             method: "GET",
@@ -37,21 +35,7 @@ export const getTokenRequest = async (endpoint) => {
         console.error("Error:", error);
         throw error;
     }
-
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
 };
-
 
 // POST
 export const postRequest = async (endpoint, data) => {
@@ -70,41 +54,39 @@ export const postRequest = async (endpoint, data) => {
         con;
         throw error;
     }
-
+};
 
 // PUT
 export const putRequest = async (endpoint, data) => {
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
+    try {
+        const response = await fetch(`${URL}/${endpoint}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
 };
 
-  
 // DELETE
 export const deleteRequest = async (endpoint) => {
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
-
+    try {
+        const response = await fetch(`${URL}/${endpoint}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
 };
 
 export default URL;
