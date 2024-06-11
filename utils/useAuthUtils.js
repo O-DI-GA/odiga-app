b2901c5da0d358a26251d2109b84077ae437d6b8
hjoo830 <hjoo830@naver.com>
2024-06-12 01:10:25 +0900

    ✨ Feat: Login status management

--- utils/useAuthUtils.js
+++ utils/useAuthUtils.js
@@ -2,11 +2,12 @@ import { postRequest } from "./api/api";
 import { storeTokens } from "./tokenUtils";
 
 // 로그인 API
-export async function loginAPI(loginData) {
+export async function loginAPI(loginData, setIsLogged) {
   console.log(`로그인 :`, loginData); // login Data 콘솔 확인
   const response = await postRequest("api/v1/user/auth/login", loginData);
   if (response.httpStatusCode === 200) {
     await storeTokens(response.data.accessToken, response.data.refreshToken);
+    setIsLogged(true);
     return "success";
   } else if (response.httpStatusCode === 400) {
     return "notMatch";
