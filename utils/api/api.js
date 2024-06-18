7145f1bc49a15ad4ff6d9992b3ed80790eb7da6d
dajeong <dajung5623@naver.com>
2024-06-18 20:34:43 +0900

    ✨ feat : change fetching Edit Profile

--- utils/api/api.js
+++ utils/api/api.js
@@ -4,66 +4,66 @@ const URL = "http://13.125.83.255:8080";
 
 // GET
 export const getRequest = async (endpoint) => {
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
+    try {
+        const response = await fetch(`${URL}/${endpoint}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
 };
 
 // GET with Token
 export const getTokenRequest = async (endpoint) => {
-  const token = await getTokenFromStorage();
-  console.log("token : ", token);
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
+    const token = await getTokenFromStorage();
+    console.log("token : ", token);
+    try {
+        const response = await fetch(`${URL}/${endpoint}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
 };
 
 export const getToken = async () => {
-  try {
-    const token = await getTokenFromStorage();
-    return token;
-  } catch (e) {
-    console.error("Error: ", e);
-    return null;
-  }
+    try {
+        const token = await getTokenFromStorage();
+        return token;
+    } catch (e) {
+        console.error("Error: ", e);
+        return null;
+    }
 };
 
 // POST
 export const postRequest = async (endpoint, data) => {
-  // console.log("확인 : ", data);
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    return response.json();
-  } catch (error) {
-    console.error("POST Error:", error);
-    con;
-    throw error;
-  }
+    // console.log("확인 : ", data);
+    try {
+        const response = await fetch(`${URL}/${endpoint}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        return response.json();
+    } catch (error) {
+        console.error("POST Error:", error);
+        con;
+        throw error;
+    }
 };
 
 // PUT
@@ -74,7 +74,7 @@ export const putRequest = async (endpoint, data) => {
         const response = await fetch(`${URL}/${endpoint}`, {
             method: "PUT",
             headers: {
-                "Content-Type": "application/json",
+                "Content-Type": "multipart/form-data",
                 Authorization: `Bearer ${token}`,
             },
             body: JSON.stringify(data),
@@ -88,18 +88,18 @@ export const putRequest = async (endpoint, data) => {
 
 // DELETE
 export const deleteRequest = async (endpoint) => {
-  try {
-    const response = await fetch(`${URL}/${endpoint}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
+    try {
+        const response = await fetch(`${URL}/${endpoint}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
 };
 
 export default URL;
