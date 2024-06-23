24b70638094886bc78e0be55c5145fbabd12063b
dajeong <dajung5623@naver.com>
2024-06-23 19:34:37 +0900

    ✨ feat : add page movement when QR code is scanned

--- component/QRScan.js
+++ component/QRScan.js
@@ -1,61 +1,88 @@
 import { BarCodeScanner } from "expo-barcode-scanner";
 import React, { useState, useEffect } from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [scanned, setScanned] = useState(false);
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        (async () => {
-            const { status } = await BarCodeScanner.requestPermissionsAsync();
-            setHasPermission(status === "granted");
-        })();
-    }, []);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-    const handleBarCodeScanned = ({ type, data }) => {
-        setScanned(true);
-        console.log(`Scanned QR code with data: ${data}`);
-    };
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
-    const toggleScanned = () => {
-        setScanned(false);
-    };
+  const handleBarCodeScanned = ({ data }) => {
+    setScanned(true);
+    console.log(`Scanned QR code with data: ${data}`);
+    const parsedData = JSON.parse(data); // Parse the JSON string
+    toPaymentScreen(parsedData);
+  };
 
-    if (hasPermission === null) {
-        return <View />;
-    }
-    if (hasPermission === false) {
-        return (
-            <View style={styles.container}>
-                <Text style={{ textAlign: "center" }}>
-                    We need your permission to access the camera
-                </Text>
-                <Button
-                    title="카메라 권한 설정"
-                    onPress={() => setHasPermission(true)}
-                />
-            </View>
-        );
-    }
+  // 결제 화면으로 이동
+  const toPaymentScreen = (paymentInfo) => {
+    navigation.navigate("Payment", {
+      storeId: paymentInfo.order.storeId,
+      orderId: paymentInfo.order.orderId,
+    });
+  };
 
+  const toggleScanned = () => {
+    setScanned(false);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
     return (
-        <View style={styles.container}>
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                style={StyleSheet.absoluteFillObject}
-            />
-            {scanned && (
-                <Button title={"Tap to Scan Again"} onPress={toggleScanned} />
-            )}
-        </View>
+      <View style={styles.permissionContainer}>
+        <Text style={{ textAlign: "center" }}>카메라 권한을 설정해주세요</Text>
+        <Button
+          title="카메라 권한 설정"
+          onPress={() => setHasPermission(true)}
+        />
+      </View>
     );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraSection}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
+      {scanned && (
+        <TouchableOpacity onPress={toggleScanned} style={styles.button}>
+          <Text style={styles.buttonText}> 인식하기 </Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "stretch",
+  },
+  cameraSection: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: "#FFD600",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 15,
+  },
+  buttonText: {
+    fontSize: 20,
+  },
 });
